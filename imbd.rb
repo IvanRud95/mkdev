39f5dc2e1760dd7706c8c6e1122a5e0932226fd8
Arthur <zhenya.brovcko@yandex.ru>
2016-12-07 07:41:12 -0800

    Complete the second task

--- imbd.rb
+++ imbd.rb
@@ -1,10 +1,29 @@
-# Считать из файла все фильмы
+def  imbd
+  if File.exist?(ARGV[0])
+    list = File.open(ARGV[0], 'r')
+  else
+    puts "A file is absent. A file will be selected by default (movies.txt)"
+    list = File.open('movies.txt', 'r')
+  end
 
-# Вывести названия и рейтинг всех фильмов, в названии которых есть слово "Max"
+  movies = []
 
-# Рейтинг нужно вывести количеством звёздочек. Поскольку у всех фильмов в топ-250 рейтинг больше 8,
-# то количество звёздочек должно соответствовать десятым долям рейтинга (для рейтинга «8.6» должна рядом с названием
-#   быть выведена строка «******»
+  list.each_line do |line|
+    movies << line.split('|')
+  end
 
-movies = ARGV[0]
+  movies.each do |movie|
+    if movie[1].include?("Max")
+      rating = movie[7].split('.')
+      rating = "*" * rating[1].to_i
+      puts "#{movie[1]} #{rating} "
+    else
+    end
+  end
+end
 
+begin
+  imbd
+rescue
+  puts "A file with movies not loaded"
+end
