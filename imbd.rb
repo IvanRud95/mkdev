13c04937340e6545f08ec02fef6419cd65683d02
Arthur <zhenya.brovcko@yandex.ru>
2016-12-09 13:42:36 -0800

    completed third task

--- imbd.rb
+++ imbd.rb
@@ -1,21 +1,13 @@
 if ARGV[0].nil?
-  puts "A file is absent or incorrect entered. A file will be selected by default (movies.txt)"
+  puts "A file is not entered or incorrect entered. A file will be selected by default (movies.txt)"
   file = "movies.txt"
 else
   File.exist?(ARGV[0]) ? file = ARGV[0] : exit()
 end
 
-list = File.open(file, 'r')
+KEYS = [:link, :title, :year, :country, :release, :genre, :time, :rating, :director, :actors]
 
-@movies = []
-
-list.each_line do |line|
-  @movies << line.split('|')
-end
-
-@movies.map! do |movie|
-  { link: movie[0], title: movie[1],  year: movie[2], country: movie[3], release: movie[4], genre: movie[5], time: movie[6], rating: movie[7], director: movie[8], actors: movie[9] }
-end
+@movies = File.read(file).each_line.map { |movie| movie.split('|') }.map { |movie| KEYS.zip(movie).to_h }
 
 def top_five_movies
   @movies.sort_by { |time| time[:time].to_i }.reverse.first(5).each do |movie|
