252e941675b53d141beaa04a59f735be3b3ffac8
Arthur <zhenya.brovcko@yandex.ru>
2016-12-11 14:36:55 -0800

    completed fourth task

--- imbd.rb
+++ imbd.rb
@@ -1,3 +1,7 @@
+require 'csv'
+require 'ostruct'
+require 'date'
+
 if ARGV[0].nil?
   puts "A file is not entered or incorrect entered. A file will be selected by default (movies.txt)"
   file = "movies.txt"
@@ -7,29 +11,34 @@ end
 
 KEYS = [:link, :title, :year, :country, :release, :genre, :time, :rating, :director, :actors]
 
-@movies = File.read(file).each_line.map { |movie| movie.split('|') }.map { |movie| KEYS.zip(movie).to_h }
+@movies = CSV.read(file, col_sep: '|' ).map { |movie| OpenStruct.new(KEYS.zip(movie).to_h) }
 
 def top_five_movies
-  @movies.sort_by { |time| time[:time].to_i }.reverse.first(5).each do |movie|
-    puts "#{movie[:title]} (#{movie[:release]}; #{movie[:genre]}) - #{movie[:time]}"
+  @movies.sort_by { |movie| movie.time.to_i }.reverse.first(5).each do |movie|
+    puts "#{movie.title} (#{movie.release}; #{movie.genre}) - #{movie.time}"
   end
 end
 
 def ten_comedies
-  @movies.select { |genre| genre[:genre].include?("Comedy") }.sort_by { |release| release[:release] }.first(10).each do |movie|
-    puts "#{movie[:title]} (#{movie[:release]}; #{movie[:genre]}) - #{movie[:time]}"
+  @movies.select { |movie| movie.genre.include?("Comedy") }.sort_by { |movie| movie.release}.first(10).each do |movie|
+    puts "#{movie.title} (#{movie.release}; #{movie.genre}) - #{movie.time}"
   end
 end
 
 def list_directors
-  puts @movies.map { |directors| directors[:director] }.uniq.sort_by { |director| director.split(' ')[-1] }
+  puts @movies.map { |movie| movie.director }.uniq.sort_by { |director| director.split(' ')[-1] }
 end
 
 def amount_movies_produced_outside_usa
-  puts @movies.map { |country| country[:country] }.delete_if { |country| country == "USA" }.count
+  puts @movies.map { |movie| movie.country }.delete_if { |country| country == "USA" }.count
+end
+
+def monthly_statistics
+  puts @movies.map(&:release).reject { |date| date.count('-').zero? }.map { |date| Date._strptime(date, '%Y-%m') }.sort_by { |date| date[:mon] }.group_by { |date| Date::MONTHNAMES[date[:mon]] }.map { |month, amount| "#{month} - #{amount.size}" }
 end
 
 top_five_movies
 ten_comedies
 list_directors
 amount_movies_produced_outside_usa
+monthly_statistics
