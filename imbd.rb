923f5b9b973b91912c1192af5a982b1f35ef615e
Arthur <zhenya.brovcko@yandex.ru>
2016-12-12 03:40:50 -0800

    completed fourth task

--- imbd.rb
+++ imbd.rb
@@ -20,17 +20,17 @@ def top_five_movies
 end
 
 def ten_comedies
-  @movies.select { |movie| movie.genre.include?("Comedy") }.sort_by { |movie| movie.release}.first(10).each do |movie|
+  @movies.select { |movie| movie.genre.include?("Comedy") }.sort_by(&:release).first(10).each do |movie|
     puts "#{movie.title} (#{movie.release}; #{movie.genre}) - #{movie.time}"
   end
 end
 
 def list_directors
-  puts @movies.map { |movie| movie.director }.uniq.sort_by { |director| director.split(' ')[-1] }
+  puts @movies.map(&:director).uniq.sort_by { |director| director.split(' ')[-1] }
 end
 
 def amount_movies_produced_outside_usa
-  puts @movies.map { |movie| movie.country }.delete_if { |country| country == "USA" }.count
+  puts @movies.map(&:country).delete_if { |country| country == "USA" }.count
 end
 
 def monthly_statistics
@@ -42,8 +42,8 @@ def monthly_statistics
    map { |month, amount| "#{month} - #{amount.size}" }
 end
 
-top_five_movies
-ten_comedies
-list_directors
-amount_movies_produced_outside_usa
+# top_five_movies
+# ten_comedies
+# list_directors
+# amount_movies_produced_outside_usa
 monthly_statistics
