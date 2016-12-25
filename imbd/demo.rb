ef2f2d406e4d9c914a6764e1747b28862a8a7216
Arthur <zhenya.brovcko@yandex.ru>
2016-12-25 14:37:45 -0800

    task5

--- imbd/demo.rb
+++ imbd/demo.rb
@@ -3,7 +3,7 @@ require 'date'
 require_relative 'moviecollection'
 require_relative 'movie'
 
-movies = MovieCollection.new('movies.txt')
+collection = MovieCollection.new('movies.txt')
 
 #puts movies.all
 #puts movies.sort_by(:length)
@@ -15,7 +15,7 @@ movies = MovieCollection.new('movies.txt')
 
 m = ["http://imdb.com/title/tt0076759/?ref_=chttp_tt_19", "Star Wars: Episode IV - A New Hope", "1977", "USA", "1977-05-25", "Action,Adventure,Fantasy", "121 min", "8.7", "George Lucas", "Mark Hamill,Harrison Ford,Carrie Fisher"]
 
-movie = Movie.new(m, movies)
+movie = Movie.new(m, collection)
 
 begin
   puts movie.has_genre?('Comedy1')
